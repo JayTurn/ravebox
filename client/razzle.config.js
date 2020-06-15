77896012c1c642f65904de69d6b8700b9626d591
The Jay <jay@altega.com.au>
2020-06-15 16:52:32 +1000

    Added performance enhancements to reduce load times.

--- client/razzle.config.js
+++ client/razzle.config.js
@@ -2,9 +2,57 @@
  * razzle.config.js
  * Webpack overrides for the application.
  */
-'use strict';
+
+// Modules.
+const path = require('path');
+const LoadableWebpackPlugin = require('@loadable/webpack-plugin');
+const LoadableBabelPlugin = require('@loadable/babel-plugin');
+const babelPresetRazzle = require('razzle/babel');
 
 module.exports = {
+  modify: (config, {dev, target}) => {
+    const isDev = dev === true;
+    const isWeb = target === 'web';
+
+    const appConfig = Object.assign({}, config);
+
+    // Run for the web client only.
+    if (isWeb) {
+      const filename = path.resolve(__dirname, 'build');
+
+      appConfig.plugins.push(
+        new LoadableWebpackPlugin({
+          outputAsset: false,
+          writeToDisk: { filename },
+        }),
+      );
+
+      appConfig.output.filename = isDev
+        ? 'static/js/[name].js'
+        : 'static/js/[name].[chunkhash:8].js';
+
+      appConfig.optimization = Object.assign({}, appConfig.optimization, {
+        runtimeChunk: true,
+        splitChunks: {
+          cacheGroups: {
+            commons: {
+              test: /[\\/]node_modules[\\/]/,
+              name: 'vendors',
+            },
+          },
+          chunks: 'all',
+          name: isDev,
+        },
+      });
+    }
+
+    return appConfig;
+  },
+  modifyBabelOptions: () => ({
+    babelrc: false,
+    presets: [babelPresetRazzle],
+    plugins: [LoadableBabelPlugin],
+  }),
   plugins: [
     {
       name: "typescript",
@@ -19,6 +67,25 @@ module.exports = {
         },
       },
     },
+    {
+      name: 'compression',
+      options: {
+        brotli: true,
+        gzip: true,
+        compressionPlugin: {
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: /\.(js|css|html|svg)$/,
+          compressionOptions: { level: 9 },
+          minRatio: 0.8,
+        },
+        brotliPlugin: {
+          asset: '[path].br[query]',
+          test: /\.(js|css|html|svg)$/,
+          minRatio: 0.8,
+        },
+      }
+    }
   ],
   rules: [
     {
--- client/src/server/index.tsx
+++ client/src/server/index.tsx
@@ -4,15 +4,29 @@
  */
 
 // Import the dependent modules.
-//import { ConnectedRouter } from 'connected-react-router';
+import {
+  ChunkExtractor,
+  ChunkExtractorManager 
+} from '@loadable/server';
+import compression from 'compression';
 import express from 'express';
-import { Frontload, frontloadServerRender } from 'react-frontload';
+import expressStaticGzip from 'express-static-gzip';
+import {
+  Frontload,
+  frontloadServerRender
+} from 'react-frontload';
+import { Helmet } from 'react-helmet';
+import {
+  html,
+  oneLineTrim
+} from 'common-tags';
+import * as Path from 'path';
+import { Provider } from 'react-redux';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import { StaticRouter } from 'react-router';
-import { Provider } from 'react-redux';
-import { Store } from 'redux';
 import Serialize from 'serialize-javascript';
+import { StaticRouter } from 'react-router';
+import { Store } from 'redux';
 
 // Import the dependent components.
 import App from '../routes/App';
@@ -33,16 +47,18 @@ const syncLoadAssets = () => {
 // Synchronize the assets from the environment variables.
 syncLoadAssets();
 
-const serverRender = async (url: string, context: any) => {
+const serverRender = async (url: string, context: any, extractor: ChunkExtractor) => {
   const markup: string = await frontloadServerRender(() => (
     renderToString(
-      <Frontload>
-        <Provider store={store}>
-          <StaticRouter location={url} context={context}>
-              <App />
-          </StaticRouter>
-        </Provider>
-      </Frontload>
+      <ChunkExtractorManager extractor={extractor}>
+        <Frontload>
+          <Provider store={store}>
+            <StaticRouter location={url} context={context}>
+                <App />
+            </StaticRouter>
+          </Provider>
+        </Frontload>
+      </ChunkExtractorManager>
     )
   ));
 
@@ -54,41 +70,53 @@ const serverRender = async (url: string, context: any) => {
  */
 const server = express()
   .disable('x-powered-by')
-  .use(express.static(process.env.RAZZLE_PUBLIC_DIR!))
+  .use(compression())
+  .use(
+    expressStaticGzip(process.env.RAZZLE_PUBLIC_DIR!, {
+      enableBrotli: true,
+      orderPreference: ['br', 'gz']
+    })
+  )
   .get('/*', async (req: express.Request, res: express.Response) => {
+    const extractor = new ChunkExtractor({
+      statsFile: Path.resolve('build/loadable-stats.json'),
+      entrypoints: ['client'],
+    });
     const context = {};
-    const markup = await serverRender(req.url, context);
+    const markup = await serverRender(req.url, context, extractor);
     const storeState: Store = store.getState();
+
+    const helmet = Helmet.renderStatic();
 
     // Send the html response to the client.
     res.status(200).send(
-      `<!doctype html>
-       <html lang="">
-       <head>
-         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-         <meta charSet='utf-8' />
-         <title>Ravebox</title>
-         <meta name="viewport" content="width=device-width, initial-scale=1">
-         <link href="https://fonts.googleapis.com/css2?family=Muli:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-         <link rel="shortcut icon" href="/favicon.ico">
-         ${
-           assets.client.css
-             ? `<link rel="stylesheet" href="${assets.client.css}">`
-             : ''
-         }
-         ${
-           process.env.NODE_ENV === 'production'
-             ? `<script src="${assets.client.js}" defer crossorigin></script>`
-             : `<script src="${assets.client.js}" defer crossorigin></script>`
-         }
-       </head>
-       <body>
-         <div id="root" class="loader">${markup}</div>
-       </body>
-       <script>
-         window.__PRELOADED_STATE__ = ${Serialize(storeState)}
-       </script>
-       </html>`
+      oneLineTrim(
+        html`
+          <!doctype html>
+           <html lang="en" ${helmet.htmlAttributes.toString()}>
+           <head>
+             <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+             <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+             <meta charSet='utf-8' />
+             ${helmet.title.toString()}
+             <meta name="viewport" content="width=device-width, initial-scale=1">
+             <link href="https://fonts.googleapis.com/css2?family=Muli:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" crossorigin>
+             <link rel="shortcut icon" href="/favicon.ico">
+             ${helmet.meta.toString()}
+             ${helmet.link.toString()}
+             ${extractor.getLinkTags()}
+             ${extractor.getStyleTags()}
+           </head>
+           <body ${helmet.bodyAttributes.toString()}>
+             <div id="root" class="loader">${markup}</div>
+             ${extractor.getScriptTags()}
+           </body>
+           <script>
+             window.__PRELOADED_STATE__ = ${Serialize(storeState)}
+           </script>
+           </html>
+        `
+      )
     );
   });
 
